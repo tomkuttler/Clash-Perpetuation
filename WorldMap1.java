139cc83bee4bd50e0900017d099a949f19e81ec0
Tiger262626 <tomkuttler@gmail.com>
2021-05-15 12:17:10 +0200

    Created classes

---
+++ WorldMap1.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class WorldMap1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WorldMap1 extends World
+{
+
+    /**
+     * Constructor for objects of class WorldMap1.
+     * 
+     */
+    public WorldMap1()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
--- WorldMap2.java
+++ WorldMap2.java
@@ -1,19 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class WorldMap2 here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class WorldMap2 extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class WorldMap2.
      * 
      */
-    public MyWorld()
+    public WorldMap2()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
