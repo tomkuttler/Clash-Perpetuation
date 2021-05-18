5f7802ddcde57b5d8b41acb5ae5e23e0fdbe7ce0
Tiger262626 <tomkuttler@gmail.com>
2021-05-18 13:46:30 +0200

    Added redPotionMaxStackSize

--- ItemData.java
+++ ItemData.java
@@ -10,18 +10,19 @@ public class ItemData
     //----- longsword -----
     private int longswordDamage = 30;
     private double longswordUseCooldown = 1000000000.0;
-    private int longswordStackSize = 1;
+    private int longswordMaxStackSize = 1;
     
     //----- RANGED WEAPONS -----
     //----- bow1 -----
     private int bow1Damage = 30;
     private double bow1UseCooldown = 1000000000.0;
-    private int bow1StackSize = 1;
+    private int bow1MaxStackSize = 1;
     private int bow1Range = 200;
     private int bow1Speed = 3;
     
     //----- HEALING ITEMS -----
     //----- redPotion -----
+    private int redPotionMaxStackSize = 64;
     private int redPotionHealthPoints = 10;
     private double redPotionUseCooldown = 1000000000.0;
     
@@ -61,15 +62,19 @@ public class ItemData
         }
     }
     
-    public int getStackSize(String item)
+    public int getMaxStackSize(String item)
     {
         if(item == "longsword")
         {
-            return longswordStackSize;
+            return longswordMaxStackSize;
         }
         else if(item == "bow1")
         {
-            return bow1StackSize;
+            return bow1MaxStackSize;
+        }
+        else if(item == "redPotion")
+        {
+            return redPotionMaxStackSize;
         }
         else
         {
